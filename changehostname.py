066b1a91913bce23852cd594ae6e1af6b495b45c
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-01 17:48:36 -0500

    fix the calling of the function from the command line

--- changehostname.py
+++ changehostname.py
@@ -11,7 +11,11 @@ def set_hostname(hostname, filepath='/etc/hostname'):
 if __name__ == "__main__":
         args = sys.argv[1:]
         l = len(args)
-        if l > 2 or l < 1:
+        if l == 2:
+                set_hostname(args[0], args[1])
+        elif l == 1:
+                set_hostname(args[0])
+        else:
                 print("""Usage : sudo python changehostname.py hostname [filepath]
                 hostname : specify the hostname to use
                 filepath : specify the hostname filepath""")
