75d3dd0caa0a558b34bd4fe4bd80dad8b2741c87
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-01 22:40:15 -0500

    change this file

--- change_hostname.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import os
-import sys
-
-os.system("chmod +x /boot/PiBakery/blocks/sethostname/sethostname.sh")
-os.system("/boot/PiBakery/blocks/sethostname/sethostname.sh "+sys.argv[1])
