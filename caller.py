370ed285c8dae5add64d0104fb443ade692b3945
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 22:13:10 -0500

    fix args concatenation

--- caller.py
+++ caller.py
@@ -5,7 +5,7 @@ import sys
 
 filename = sys.argv[1]
 filename = filename if filename[:2] == './' else ''.join(['./', filename])
-args = [str(i) for i in sys.argv[2:]]
+args = ' '.join([str(i) for i in sys.argv[2:]])
 
 os.system(''.join(['sudo chmod u+x ', filename]))
-os.system(''.join(['sudo ', filename]+args))
+os.system(''.join(['sudo ', filename, args]))
