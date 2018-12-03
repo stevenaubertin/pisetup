e174a1aec5b20760c53dd71e25b6278ff6d3b328
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 22:25:55 -0500

    remove useless files

--- hostname/change_hostname.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python
-
-import os
-import sys
-
-os.system('chmod u+x ./change_hostname.sh')
-os.system('sudo ./change_hostname.sh '+sys.argv[1])
--- update/update.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python
-
-import os
-import sys
-
-os.system('chmod u+x ./update.sh')
-os.system('sudo ./update.sh '+sys.argv[1])
