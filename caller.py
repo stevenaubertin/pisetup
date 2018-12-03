a2797fef08ce5bfb1791112966c5a77015ed0116
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 22:19:56 -0500

    fix missing blank space when calling an other script

--- caller.py
+++ caller.py
@@ -8,4 +8,4 @@ filename = filename if filename[:2] == './' else ''.join(['./', filename])
 args = ' '.join([str(i) for i in sys.argv[2:]])
 
 os.system(''.join(['sudo chmod u+x ', filename]))
-os.system(''.join(['sudo ', filename, args]))
+os.system(''.join(['sudo ', filename, ' ', args]))
