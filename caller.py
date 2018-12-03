f1ca0eefbab61fc9d31d7eb76988b16a484874b8
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 21:50:06 -0500

    fix a bug where the arguments where of the wrong type

--- caller.py
+++ caller.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 filename = sys.argv[1]
-args = sys.argv[2:]
+args = [str(i) for i in sys.argv[2:]]
 
 os.system(''.join(['sudo chmod u+x ', filename]))
-os.system(''.join(['sudo', filename, args]))
+os.system(''.join(['sudo', filename]+args))
