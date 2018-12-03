8a61aeb7de5a73df0163748e96d9a04ce9814780
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 21:42:25 -0500

    added caller.py wich enables calling scripts from python

---
+++ caller.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+import os
+import sys
+
+filename = sys.argv[1]
+args = sys.argv[2:]
+
+os.system(''.join(['sudo chmod u+x ', filename]))
+os.system(''.join(['sudo', filename, args]))
