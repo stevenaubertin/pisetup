88831505ee6d2feca91a96031566432e02bdaf85
Steven Aubertin <saubertin@GENETEC.COM>
2018-12-02 21:58:56 -0500

    add ./ when the executing file is missing one

--- caller.py
+++ caller.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 filename = sys.argv[1]
+filename = filename if filename[:2] == './' else ''.join(['./', filename])
 args = [str(i) for i in sys.argv[2:]]
 
 os.system(''.join(['sudo chmod u+x ', filename]))
