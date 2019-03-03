65b53e38adee9faf68c84333fb7f4a5e1d1658bb
Steven Aubertin <stevenaubertin@gmail.com>
2019-03-03 16:54:41 -0500

    Update caller script to log call python and sh files

--- common/caller.py
+++ common/caller.py
@@ -2,10 +2,36 @@
 
 import os
 import sys
+import subprocess
+import logging
 
-def call(filename, args):
-    filename = filename if filename[:2] == './' else ''.join(['./', filename])
-    args = ' '.join([str(i) for i in args[2:]])
+class Caller:
+    def __init__(self, logger=None):
+        self.logger = logger if logger else logging
 
-    os.system(''.join(['sudo chmod +x ', filename]))
-    os.system(''.join(['sudo ', filename, ' ', args]))
+    def __error(self, cmd, ex):
+        self.logger.error('calling \"{}\" produced : \"{}\"'.format(cmd, ex))
+
+    def __os_call(self, *argv):
+        try:
+            self.logger.info(subprocess.check_output(argv))
+        except Exception as ex:
+            self.__error(' '.join([str(i) for i in argv]), ex)
+
+    def call_sh(self, filename, *argv):
+        self.__os_call('sudo', 'chmod', '+x', filename)
+        self.__os_call('su', '-', 'pi', '-c', filename)
+
+    def call_python(self, filename, *argv):
+        self.__os_call(['su', '-', 'pi', '-c', '"python \"{}\""'.format(filename)]+argv)
+
+    def call(self, filename, *argv):
+        self.logger.info('calling \"{}\" with args {}'.format(filename, ' '.join([str(i) for i in argv])))
+
+        filename = filename if filename[:2] == './' else ''.join(['./', filename])
+        argv = ' '.join([str(i) for i in argv])
+
+        if filename[:-2] == '.py':
+            self.call_python(filename, argv)
+        else:
+            self.call_sh(filename, argv)
