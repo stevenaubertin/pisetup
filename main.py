df68d6f027b24a487d7095ec2bd1552b73aa7946
Steven Aubertin <stevenaubertin@gmail.com>
2019-03-03 16:55:00 -0500

    Update main.py to run configurations and scripts

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 #!/bin/python
-from common.caller import 
+
+import os
+import json
+import logging
+from common.caller import Caller
+
+# Loads configurations
+with open('./config.json', 'r') as fp:
+    configs = json.load(fp)
+
+# Setup logger and helper
+logging.basicConfig(format=configs['logformat'], filename=configs['logfile'], level=logging.INFO)
+caller = Caller(logging)
 
 # Setup localization
-
+caller.call('./localization/locale.sh', configs['locale'])
+caller.call('./localization/timezone.sh', configs['timezone'])
 
 # Setup Network
+caller.call('./network/toggle_ssh.sh', configs['toggle_ssh'])
 
-# Setup Optimize
+# Setup Optimization
+caller.call('./hardware/disable_swap.sh', configs['disable_swap'])
+caller.call('./hardware/memsplit.sh', configs['memsplit'])
 
 # Install Packages
+
+
+# Reboot
+os.system('reboot')
