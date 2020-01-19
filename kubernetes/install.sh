84e37e2aefe9a88ea50a71889179ded7999e1f89
Steven Aubertin <stevenaubertin@gmail.com>
2020-01-19 17:41:46 -0500

    Remove useless stuff

--- kubernetes/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
-sudo apt-get update -q && \
-sudo apt-get install -qy kubeadm
