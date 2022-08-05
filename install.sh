f3e4881aa56ea7cf632ed7ba28cc7b79e589f64d
Steven Aubertin <stevenaubertin@gmail.com>
2022-08-05 18:11:03 -0400

    Update install.sh
    add echo to install file

--- install.sh
+++ install.sh
@@ -4,33 +4,39 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Installing Packages
+echo "Installing Packages"
 sudo apt-get install tree -y
 sudo apt-get install wget -y
 sudo apt-get install git -y
 sudo apt-get install nano -y
 
+# Disable swap
 echo Disabling swap
 sudo dphys-swapfile swapoff && \
 sudo dphys-swapfile uninstall && \
 sudo update-rc.d dphys-swapfile remove
 echo Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
 
+echo "Mem split 16"
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 sudo raspi-config nonint do_memory_split 16
 
 # Install Docker and Docker-Compose
+echo "Installing docker and docker compose"
 curl -sSL get.docker.com | sh && sudo usermod x0r -aG docker
 #sudo apt-get install libffi-dev libssl-dev
 #sudo apt install python3-dev
 #sudo apt-get install -y python3 python3-pip
 sudo apt-get install docker-compose -y
 
+echo "docker deamon on start"
 sudo systemctl start docker
 
 # Setup locals
 loc="en_CA.UTF-8"
+echo "setup local $loc"
 sed -i 's/^# *\('"loc"'\)/\1/' /etc/locale.gen
 locale-gen --purge
 locale-gen "loc UTF-8"
