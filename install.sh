998966b5a8e671bcd0f4660453ac8068732bcce4
Steven Aubertin <stevenaubertin@gmail.com>
2022-08-05 18:09:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
+# Update packages sources
 sudo apt-get update && sudo apt-get upgrade -y
 
+# Installing Packages
 sudo apt-get install tree -y
 sudo apt-get install wget -y
 sudo apt-get install git -y
 sudo apt-get install nano -y
 
-curl -sSL get.docker.com | sh && sudo usermod x0r -aG docker
+echo Disabling swap
+sudo dphys-swapfile swapoff && \
+sudo dphys-swapfile uninstall && \
+sudo update-rc.d dphys-swapfile remove
+echo Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
 
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
+sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
+echo $orig | sudo tee /boot/cmdline.txt
+sudo raspi-config nonint do_memory_split 16
+
+# Install Docker and Docker-Compose
+curl -sSL get.docker.com | sh && sudo usermod x0r -aG docker
+#sudo apt-get install libffi-dev libssl-dev
+#sudo apt install python3-dev
+#sudo apt-get install -y python3 python3-pip
 sudo apt-get install docker-compose -y
 
-sudo systemctl to enable Docker
+sudo systemctl start docker
+
+# Setup locals
+loc="en_CA.UTF-8"
+sed -i 's/^# *\('"loc"'\)/\1/' /etc/locale.gen
+locale-gen --purge
+locale-gen "loc UTF-8"
+update-locale LANG="loc"
+dpkg-reconfigure -f noninteractive locales
+echo "locale will be updated after reboot"
+echo "default value : $(cat /etc/default/locale | grep LANG)"
 
 echo "Change locale and hostname"
-sudo raspi-config
+#sudo raspi-config
+
