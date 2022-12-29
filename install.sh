b1e254ad4024d1c2b34ab5a242d774ef5f223451
Steven Aubertin <stevenaubertin@Scarlet4.bananadomain>
2022-12-29 16:27:58 -0500

    update hostname

--- install.sh
+++ install.sh
@@ -66,7 +66,10 @@ echo '###################################'
 echo "hostname"
 mac=${python ./network/getmac.py}
 hostname=${python ./hostname/get_hostname.py "$USERNAME" "$PASSWORD" $mac}
-
+echo "hostname found is $hostname"
+sudo raspi-config nonint do_hostname "$hostname"
+sudo hostname -b "$hostname"
+sudo systemctl restart avahi-daemon
 
 sudo reboot
 
